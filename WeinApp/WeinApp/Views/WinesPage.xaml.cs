08c7319b9304161d8a703f268c717eb773ccd447
Sven Ijsseling <sven.ijsseling@outlook.com>
2021-07-05 15:43:31 +0200

    as

--- WeinApp/WeinApp/Views/WinesPage.xaml.cs
+++ WeinApp/WeinApp/Views/WinesPage.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WeinApp.Models;
 using WeinApp.ViewModels;
-using WeinApp.Views;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace WeinApp.Views
 {
