46b03ed34f22ccaaa766e6e02dfa5369755b4fda
Sven Ijsseling <sven.ijsseling@outlook.com>
2021-07-07 21:18:38 +0200

    SS

--- WeinApp/WeinApp/ViewModels/NewWineViewModel.cs
+++ WeinApp/WeinApp/ViewModels/NewWineViewModel.cs
@@ -8,57 +8,57 @@ namespace WeinApp.ViewModels
 {
     public class NewWineViewModel : BaseViewModel
     {
-            private string weinname;
-            private string jahrgang;
+        private string weinname;
+        private string jahrgang;
 
-            public NewWineViewModel()
+        public NewWineViewModel()
+        {
+            SaveCommand = new Command(OnSave, ValidateSave);
+            CancelCommand = new Command(OnCancel);
+            this.PropertyChanged +=
+                (, _) => SaveCommand.ChangeCanExecute();
+        }
+
+        private bool ValidateSave()
+        {
+            return !String.IsNullOrWhiteSpace(weinname)
+                && !String.IsNullOrWhiteSpace(jahrgang);
+        }
+
+        public string Weinname
+        {
+            get => weinname;
+            set => SetProperty(ref weinname, value);
+        }
+
+        public string Jahrgang
+        {
+            get => jahrgang;
+            set => SetProperty(ref jahrgang, value);
+        }
+
+        public Command SaveCommand { get; }
+        public Command CancelCommand { get; }
+
+        private async void OnCancel()
+        {
+            // This will pop the current page off the navigation stack
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnSave()
+        {
+            Wine newWine = new Wine()
             {
-                SaveCommand = new Command(OnSave, ValidateSave);
-                CancelCommand = new Command(OnCancel);
-                this.PropertyChanged +=
-                    (, _) => SaveCommand.ChangeCanExecute();
-            }
+                Id = Guid.NewGuid().ToString(),
+                Weinname = Weinname,
+                Jahrgang = Jahrgang
+            };
 
-            private bool ValidateSave()
-            {
-                return !String.IsNullOrWhiteSpace(weinname)
-                    && !String.IsNullOrWhiteSpace(jahrgang);
-            }
+            await DataStore.AddItemAsync(newWine);
 
-            public string Weinname
-            {
-                get => weinname;
-                set => SetProperty(ref weinname, value);
-            }
-
-            public string Jahrgang
-            {
-                get => jahrgang;
-                set => SetProperty(ref jahrgang, value);
-            }
-
-            public Command SaveCommand { get; }
-            public Command CancelCommand { get; }
-
-            private async void OnCancel()
-            {
-                // This will pop the current page off the navigation stack
-                await Shell.Current.GoToAsync("..");
-            }
-
-            private async void OnSave()
-            {
-                Wine newWine = new Wine()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Weinname = Weinname,
-                    Jahrgang = Jahrgang
-                };
-
-                await DataStore.AddItemAsync(newWine);
-
-                // This will pop the current page off the navigation stack
-                await Shell.Current.GoToAsync("..");
-            }
+            // This will pop the current page off the navigation stack
+            await Shell.Current.GoToAsync("..");
         }
     }
+}
