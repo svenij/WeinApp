b5049e820ed55cd2ad047322aa8c76bd0f59c996
Sven Ijsseling <sven.ijsseling@outlook.com>
2021-07-07 19:44:49 +0200

    remove

--- WeinApp/WeinApp/Core/ContainerExtensions.cs
+++ WeinApp/WeinApp/Core/ContainerExtensions.cs
@@ -1,35 +1,7 @@
-﻿using JetBrains.Annotations;
-using SimpleInjector;
-using System;
-using WeinApp.Models;
-using WeinApp.Services;
-
-
-namespace WeinApp.Core
+﻿namespace WeinApp.Core
 {
     public static class ContainerExtensions
     {
-        //public static Container CreateContainer()
-        //{
-        //    return new Container()
-        //    {
-        //        Options =
-        //{
-        //  ResolveUnregisteredConcreteTypes = true,
-        //  AllowOverridingRegistrations = true
-        //}
-        //    }.RegisterWineServices();
-        //}
-
-        //public static Container RegisterWineServices([NotNull] this Container container)
-        //{
-        //    if (container is null) { throw new ArgumentNullException(nameof(container)); }
-
-        //    container.RegisterSingleton<IDataStore<Wine>, WineDataStore>();
-          
-        //    container.RegisterSingleton<IWineSaver, WineSaver>();
-
-        //    return container;
-        //}
+       
     }
 }
--- WeinApp/WeinApp/Models/UniqueItem.cs
+++ WeinApp/WeinApp/Models/UniqueItem.cs
@@ -5,10 +5,6 @@ namespace WeinApp.Models
 {
     public class UniqueItem
     {
-        // Another way to create a primary key is to use an integer and ask the database to auto-increment it
-        // [PrimaryKey, AutoIncrement]
-        // public int Id { get; set; }
-
         [PrimaryKey]
         public string Id { get; set; } = Guid.NewGuid().ToString();
     }
